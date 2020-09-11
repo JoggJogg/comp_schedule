a2aac043d1bc92a19256298bc4ded37c2451cff0
Jochem Dobber <JM.Dobber@student.han.nl>
2020-09-11 15:27:46 +0200

    first working version along with readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # comp_schedule
 Personal schedule calculator for my track competitions
+
+---
+## Requirements
+
+You will need NodeJS to run the project.
+
+## Install
+
+    $ git clone https://github.com/JoggJogg/comp_schedule
+    $ cd comp_schedule
+    $ npm install
+
+## Running the project
+
+  ### Arguments
+
+    $ npm start <competition> <callroom> <travel>
+
+  ### Example
+    $ npm start 19:04 25 10
+
+  
+  ### Output
+    16:04 - bicarbonaat
+    16:49 - bicarbonaat
+    17:29 - to track
+    17:34 - bicarbonaat
+    17:39 - warmup
+    18:19 - caffeine
+    18:39 - callroom 
+    19:04 - race 
+
+ 
+
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+const moment = require('moment')
+
+const args = process.argv.slice(2);
+
+if(args.length == 3) {
+  
+  const callRoom = args[1]
+  const travelDuration = args[2]
+
+  let timeStamps = []
+
+  let competitionMoment = moment(args[0], 'H-m')
+  timeStamps.push(competitionMoment.format("HH:mm [- race] "))
+
+  let callRoomMoment = moment(args[0], 'H-m').subtract(callRoom, 'minutes')
+  timeStamps.push(callRoomMoment.format("HH:mm [- callroom ]"))
+
+  let caffeineMoment = moment(args[0], 'H-m').subtract(45, 'minutes')
+  timeStamps.push(caffeineMoment.format("HH:mm [- caffeine]"))
+
+  let warmupMoment = callRoomMoment.subtract(60, 'minutes')
+  timeStamps.push(warmupMoment.format("HH:mm [- warmup]"))
+
+  let travelMoment = warmupMoment.subtract(travelDuration, 'minutes')
+  timeStamps.push(travelMoment.format("HH:mm [- to track]"))
+  
+  let bicarbonaatMoment3 = competitionMoment.subtract(90, 'minutes')
+  timeStamps.push(bicarbonaatMoment3.format("HH:mm [- bicarbonaat]"))
+  
+  let bicarbonaatMoment2 = bicarbonaatMoment3.subtract(45, 'minutes')
+  timeStamps.push(bicarbonaatMoment2.format("HH:mm [- bicarbonaat]"))
+
+  let bicarbonaatMoment1 = bicarbonaatMoment2.subtract(45, 'minutes')
+  timeStamps.push(bicarbonaatMoment1.format("HH:mm [- bicarbonaat]"))
+ 
+  timeStamps.reverse()
+  timeStamps.sort()
+  
+  // timecompetition.subtract(, 'minutes').format("[bicarbonaat -] H:m")
+  // const bicarbonaat_2 = competitionStart.subtract(, 'minutes').format("[bicarbonaat -] H:m")
+  // const bicarbonaat_1 = competitionStart.subtract(, 'minutes').format("[bicarbonaat -] H:m")
+ 
+  timeStamps.forEach(timeStamp => {
+    console.log(timeStamp)
+  })
+
+} else {
+  console.log('Please use: npm start <hour:minute> <callroom> <travelDuration> ')
+}
+
